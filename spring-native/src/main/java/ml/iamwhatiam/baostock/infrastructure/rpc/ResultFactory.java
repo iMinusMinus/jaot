0f0524b9f3806cdcf24add38eb88571a6b856d55
iMinusMinus <mean.leung@outlook.com>
2022-03-18 21:25:17 +0800

    traditional style: add logback, actuator, switch compitable with java8

--- spring-native/src/main/java/ml/iamwhatiam/baostock/infrastructure/rpc/ResultFactory.java
+++ spring-native/src/main/java/ml/iamwhatiam/baostock/infrastructure/rpc/ResultFactory.java
@@ -5,19 +5,24 @@ public final class ResultFactory {
     public static BaoStockResponse newInstance(String[] headerArray, String[] bodyArray) {
         BaoStockResponse result;
         switch (headerArray[1]) {
-            case Constants.MESSAGE_TYPE_LOGIN_RESPONSE ->
+            case Constants.MESSAGE_TYPE_LOGIN_RESPONSE:
                     result = new LoginResponse(headerArray, bodyArray);
-            case Constants.MESSAGE_TYPE_QUERYALLSTOCK_RESPONSE ->
+                    break;
+            case Constants.MESSAGE_TYPE_QUERYALLSTOCK_RESPONSE:
                     result = new QueryAllStockResponse(headerArray, bodyArray);
-            case Constants.MESSAGE_TYPE_QUERYSTOCKBASIC_RESPONSE ->
+                    break;
+            case Constants.MESSAGE_TYPE_QUERYSTOCKBASIC_RESPONSE:
                     result = new QueryStockBasicResponse(headerArray, bodyArray);
-            case Constants.MESSAGE_TYPE_QUERYSTOCKINDUSTRY_RESPONSE ->
+                    break;
+            case Constants.MESSAGE_TYPE_QUERYSTOCKINDUSTRY_RESPONSE:
                     result = new QueryStockIndustryResponse(headerArray, bodyArray);
+                    break;
 
-            case Constants.MESSAGE_TYPE_GETKDATAPLUS_RESPONSE ->
+            case Constants.MESSAGE_TYPE_GETKDATAPLUS_RESPONSE:
                     result = new QueryHistoryKDataPlusResponse(headerArray, bodyArray);
+                    break;
             // TODO
-            default -> result = new BaoStockResponse(headerArray, bodyArray);
+            default: result = new BaoStockResponse(headerArray, bodyArray);
         }
 
         return result;

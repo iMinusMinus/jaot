7884f890a80212876ac7b95b163021b2bbcb52da
iMinusMinus <mean.leung@outlook.com>
2022-03-30 21:51:26 +0800

    tx & cache support

--- javaweb/src/main/java/ml/iamwhatiam/baostock/Application.java
+++ javaweb/src/main/java/ml/iamwhatiam/baostock/Application.java
@@ -2,10 +2,14 @@ package ml.iamwhatiam.baostock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableTransactionManagement
+@EnableCaching
 public class Application {
 
     public static void main(String[] args) {

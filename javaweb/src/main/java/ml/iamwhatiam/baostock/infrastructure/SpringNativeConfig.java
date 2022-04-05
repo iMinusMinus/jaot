710a752f5924e15607fdb464f92a99dfaf36b5eb
iMinusMinus <mean.leung@outlook.com>
2022-04-05 11:10:52 +0800

    fix: No serializer found for class ml.iamwhatiam.baostock.infrastructure.rpc.QueryHistoryKDataPlusResponse$Quotation and no properties discovered to create BeanSerializer;  fix: java.lang.NoSuchMethodException: 'ofNanoOfDay'  feature: add options

--- javaweb/src/main/java/ml/iamwhatiam/baostock/infrastructure/SpringNativeConfig.java
+++ javaweb/src/main/java/ml/iamwhatiam/baostock/infrastructure/SpringNativeConfig.java
@@ -18,13 +18,12 @@ import io.netty.channel.ChannelOutboundHandlerAdapter;
 import io.netty.handler.codec.ByteToMessageDecoder;
 import io.netty.handler.codec.MessageToByteEncoder;
 import io.netty.util.ReferenceCountUtil;
+import ml.iamwhatiam.baostock.infrastructure.rpc.BaoStockResponse;
 import ml.iamwhatiam.baostock.infrastructure.rpc.NettyClient;
 import ml.iamwhatiam.baostock.infrastructure.rpc.QueryHistoryKDataPlusResponse;
 import ml.iamwhatiam.baostock.infrastructure.web.StockVO;
-import ml.iamwhatiam.baostock.infrastructure.web.WrapResponseBodyAdvice;
 import org.fusesource.jansi.WindowsAnsiOutputStream;
 import org.fusesource.jansi.internal.Kernel32;
-import org.mybatis.spring.nativex.MyBatisResourcesScan;
 import org.springframework.aot.context.bootstrap.generator.infrastructure.nativex.NativeConfigurationRegistry;
 import org.springframework.nativex.AotOptions;
 import org.springframework.nativex.hint.FieldHint;
@@ -35,8 +34,18 @@ import org.springframework.nativex.hint.TypeAccess;
 import org.springframework.nativex.hint.TypeHint;
 import org.springframework.nativex.type.NativeConfiguration;
 import org.springframework.util.ClassUtils;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.Locale;
 
 // use logback-spring style and delete above, refer org.springframework.boot.logging.LogbackHints
@@ -86,20 +95,28 @@ import java.util.Locale;
         }, access = TypeAccess.QUERY_DECLARED_METHODS),
         @TypeHint(types = {
                 PooledByteBufAllocator.class,
-        })})
+        })}, options = {"-Dbaostock.socket.readTimeout"})
 // mybatis. mapper file conversion should change to p/k/g/MapperInterface.xml, and mybatis-spring-native would automatically add mapper xml file as resource file.
-@MyBatisResourcesScan(typeAliasesPackages = "ml.iamwhatiam.baostock.infrastructure.dao", mapperLocationPatterns = "mapper/**/*Mapper.xml")
+//@MyBatisResourcesScan(mapperLocationPatterns = "mapper/**/*Mapper.xml")
 // web
 @NativeHint(trigger = ResponseBodyAdvice.class, serializables = {
         @SerializationHint(types = {
-                WrapResponseBodyAdvice.Wrapper.class,
+                Integer.class,
+                String.class,
+                LocalDate.class,
+                LocalDateTime.class,
+                BigDecimal.class,
                 StockVO.class,
+                BaoStockResponse.class,
                 QueryHistoryKDataPlusResponse.class,
-                QueryHistoryKDataPlusResponse.Quotation.class,
-                QueryHistoryKDataPlusResponse.Frequency.class,
-                StackTraceElement.class,
-                StackTraceElement[].class
-        })
+        }, typeNames = { // nested types must be specific using a $ separator
+                "ml.iamwhatiam.baostock.infrastructure.web.WrapResponseBodyAdvice$Wrapper",
+                "ml.iamwhatiam.baostock.infrastructure.rpc.QueryHistoryKDataPlusResponse$Quotation",
+                "ml.iamwhatiam.baostock.infrastructure.rpc.QueryHistoryKDataPlusResponse$Frequency"
+        })}, types = {
+        @TypeHint(typeNames = {
+                "ml.iamwhatiam.baostock.infrastructure.rpc.QueryHistoryKDataPlusResponse$Quotation",
+        }, access = {TypeAccess.DECLARED_METHODS, TypeAccess.PUBLIC_METHODS})
 })
 public class SpringNativeConfig implements NativeConfiguration {
 
@@ -114,6 +131,47 @@ public class SpringNativeConfig implements NativeConfiguration {
                         jniConfiguration.forType(Kernel32.CONSOLE_SCREEN_BUFFER_INFO.class).withAccess(TypeAccess.DECLARED_FIELDS);
                         jniConfiguration.forType(Kernel32.COORD.class).withAccess(TypeAccess.DECLARED_FIELDS);
                         jniConfiguration.forType(Kernel32.SMALL_RECT.class).withAccess(TypeAccess.DECLARED_FIELDS);
+                }
+                if(ClassUtils.isPresent("org.h2.util.LocalDateTimeUtils", null)) {
+                        Method ofNanoOfDay = ReflectionUtils.findMethod(LocalTime.class, "ofNanoOfDay", long.class);
+                        Method toNanoOfDay = ReflectionUtils.findMethod(LocalTime.class, "toNanoOfDay");
+                        Method parse = ReflectionUtils.findMethod(LocalTime.class, "parse", CharSequence.class);
+                        registry.reflection().forType(LocalTime.class).withQueriedExecutables(ofNanoOfDay).withExecutables(ofNanoOfDay);
+                        registry.reflection().forType(LocalTime.class).withQueriedExecutables(toNanoOfDay).withExecutables(toNanoOfDay);
+                        registry.reflection().forType(LocalTime.class).withQueriedExecutables(parse).withExecutables(parse);
+
+                        registry.reflection().forType(LocalDate.class).withAccess(TypeAccess.QUERY_DECLARED_METHODS, TypeAccess.DECLARED_METHODS);
+
+                        Method getEpochSecond = ReflectionUtils.findMethod(Instant.class, "getEpochSecond");
+                        Method getNano = ReflectionUtils.findMethod(Instant.class, "getNano");
+                        registry.reflection().forType(Instant.class).withQueriedExecutables(getEpochSecond).withExecutables(getEpochSecond);
+                        registry.reflection().forType(Instant.class).withQueriedExecutables(getNano).withExecutables(getNano);
+
+                        Method plusNanos = ReflectionUtils.findMethod(LocalDateTime.class, "plusNanos", long.class);
+                        Method toLocalDate = ReflectionUtils.findMethod(LocalDateTime.class, "toLocalDate");
+                        Method toLocalTime = ReflectionUtils.findMethod(LocalDateTime.class, "toLocalTime");
+                        Method parseOfDateTime = ReflectionUtils.findMethod(LocalDateTime.class, "parse", CharSequence.class);
+                        registry.reflection().forType(LocalDateTime.class).withQueriedExecutables(plusNanos).withExecutables(plusNanos);
+                        registry.reflection().forType(LocalDateTime.class).withQueriedExecutables(toLocalDate).withExecutables(toLocalDate);
+                        registry.reflection().forType(LocalDateTime.class).withQueriedExecutables(toLocalTime).withExecutables(toLocalTime);
+                        registry.reflection().forType(LocalDateTime.class).withQueriedExecutables(parseOfDateTime).withExecutables(parseOfDateTime);
+
+                        Method ofTotalSeconds = ReflectionUtils.findMethod(ZoneOffset.class, "ofTotalSeconds", int.class);
+                        Method getTotalSeconds = ReflectionUtils.findMethod(ZoneOffset.class, "getTotalSeconds");
+                        registry.reflection().forType(ZoneOffset.class).withQueriedExecutables(ofTotalSeconds).withExecutables(ofTotalSeconds);
+                        registry.reflection().forType(ZoneOffset.class).withQueriedExecutables(getTotalSeconds).withExecutables(getTotalSeconds);
+
+                        Method toLocalDateTime = ReflectionUtils.findMethod(OffsetDateTime.class, "toLocalDateTime");
+                        Method getOffset = ReflectionUtils.findMethod(OffsetDateTime.class, "getOffset");
+                        Method of = ReflectionUtils.findMethod(OffsetDateTime.class, "of", LocalDateTime.class, ZoneOffset.class);
+                        Method parseOffsetDateTime = ReflectionUtils.findMethod(OffsetDateTime.class, "parse", CharSequence.class);
+                        registry.reflection().forType(OffsetDateTime.class).withQueriedExecutables(toLocalDateTime).withExecutables(toLocalDateTime);
+                        registry.reflection().forType(OffsetDateTime.class).withQueriedExecutables(getOffset).withExecutables(getOffset);
+                        registry.reflection().forType(OffsetDateTime.class).withQueriedExecutables(of).withExecutables(of);
+                        registry.reflection().forType(OffsetDateTime.class).withQueriedExecutables(parseOffsetDateTime).withExecutables(parseOffsetDateTime);
+
+                        Method toInstant = ReflectionUtils.findMethod(Timestamp.class, "toInstant");
+                        registry.reflection().forType(Timestamp.class).withQueriedExecutables(toInstant).withExecutables(toInstant);
                 }
         }
 }
